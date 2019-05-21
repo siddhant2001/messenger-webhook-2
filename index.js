636b8a01cc4637fd6636397a16a397f7ee93ee04
siddhant2001 <siddhant.jain120@gmail.com>
2019-05-21 23:36:17 +0530

    3rd commit

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "qwertyuiop"
+    let VERIFY_TOKEN = "a"
       
     // Parse the query params
     let mode = req.query['hub.mode'];
